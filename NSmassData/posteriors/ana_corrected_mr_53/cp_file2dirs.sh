f8815eda0e9c932e2616fdf6ff6a785a39aea3b4
zhiqiang.You <48940522+youzhiqiang1990@users.noreply.github.com>
2024-12-11 12:48:49 +0800

    Delete NSmassData directory

--- NSmassData/posteriors/ana_corrected_mr_53/cp_file2dirs.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-##dic need -r, while file no -r 
-for k in `ls`; do cp slurm.sh ${k}/; done
-for k in `ls`; do cp hyper.py ${k}/; done
--- NSmassData/posteriors/phe_corrected_mrms_87/cp_file2dirs.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-##dic need -r, while file no -r 
-for k in `ls`; do cp hyper.py ${k}/; done
